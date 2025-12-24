e461eea4087baf84962849d4a41c70658d110a01
Katlego Tamenti <27110966@sun.ac.za>
2025-12-25 01:06:51 +0200

    Update toralize.c

--- toralize.c
+++ toralize.c
@@ -1,27 +1,34 @@
 /* toralize.c */
-#include <"toralize.h">
 
-/*
+#include "toralize.h"
 
-./toralize 1.2.3.4 80
+Req *request(struct sockaddr_in *sock2) {
+    Req *req;
 
-*/
+    req = malloc(reqsize);
 
-int main(int argc, char *argv[]) {
-    char *host;
-    int port, s;
+    req->vn = 4;
+    req->cd = 1;
+    req->dstport = sock2->sin_port;
+    req->dstip = sock2->sin_addr.s_addr;
+    strncpy(req->userid, USERNAME, 8);
+
+    return req;
+}
+
+int connect(int s2, const struct sockaddr *sock2,
+        socklen_t addrlen) {
+
+    int s;
     struct sockaddr_in sock;
+    Req *req;
+    Res *res;
+    char buf[ressize];
+    int success;
+    char tmp[512];
+    int (*p)(int, const struct sockaddr*, socklen_t);
 
-    if (agrc < 3) {
-        fprint(stderr, "Usage: %s <host> <port>\n",
-        argv[0])
-
-        return -1;
-    }
-
-    host = argv[1];
-    port = atoi(argv[2]);
-
+    p = dlsym(RTLD_NEXT, "connect");
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0) {
         perror("socket");
@@ -33,17 +40,42 @@ int main(int argc, char *argv[]) {
     sock.sin_port = htons(PROXYPORT);
     sock.sin_addr.s_addr = inet_addr(PROXY);
 
-    if (connect(s, (struct sockaddr *)&sock, sizeof(sock))) {
+    if (p(s, (struct sockaddr *)&sock, sizeof(sock))) {
         perror("connect");
 
         return -1;
     }
 
     printf("Connected to proxy\n");
-    close(s);
+    req = request((struct sockaddr_in *)sock2);
+    write(s, req, reqsize);
+
+    memset(buf, 0, ressize);
+    if (read(s, buf, ressize) < 1) {
+        perror("read");
+        free(req);
+        close(s);
+
+        return -1;
+    }
+
+    res = (Res *)buf;
+    success = (res->cd == 90);
+    if (!success) {
+        fprintf(stderr, "Unable to traverse" 
+            "the proxy, error code: %d\n",
+            res->cd);
+
+            close(s);
+            free(req);
+
+            return -1;
+    }
+
+    printf("Connected through the proxy.\n");
+
+    dup2(s, s2);
+    free(req);
 
     return 0;
-
 }
-
-// ./toralize 1.2.3.4 80
